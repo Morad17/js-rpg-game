223a7d935f3682e867ef9653b2361f9b1a2daa44
Morad17 <morad123@hotmail.co.uk>
2022-04-21 15:55:38 +0100

    up

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ canvas.height = 576
 c.fillStyle='white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-console.log(c)
+const image = new Image()
+image.src = './img/rpgGameMap.png'
+
+
+image.onLoad = () => {
+  c.drawImage(image,0 ,0)  
+}
+
+console.log(image)
